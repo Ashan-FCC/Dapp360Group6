86a4f006de172269b87021e5fb766f507ec3a2cf
Ashan Perera <20638666+Ashan-FCC@users.noreply.github.com>
2022-08-19 13:18:35 +0700

    Error handle on inserting (#2)
    * try catch
    
    * Error handling on insert

--- src/StockPrices/App.hs
+++ src/StockPrices/App.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE ScopedTypeVariables  #-}
 module StockPrices.App (runApp, getDay) where
 
-import StockPrices.Repository (retrieveTickerPrice, insertTickerHistory)
+import StockPrices.Repository (retrieveTickerPrice, createTicker)
 import StockPrices.Database (getDbConnection)
 import StockPrices.Environment (readConfig)
 import StockPrices.Model.TickerHistory (TickerHistory(..))
@@ -12,7 +12,7 @@ runApp :: IO ()
 runApp = do
   config <- readConfig
   conn   <- getDbConnection config
-  xs     <- insertTickerHistory conn (TickerHistory { ticker = "AAPL", date = (getDay "2022-08-12"),  adjustedClose = 100,   closeP = 1000,   high = 1001,  low = 989,   open = 990,   volume = 10000 })
+  xs     <- createTicker conn (TickerHistory { ticker = "AAPL", date = (getDay "2022-08-12"),  adjustedClose = 100,   closeP = 1000,   high = 1001,  low = 989,   open = 990,   volume = 10000 })
   res    <- retrieveTickerPrice conn "NFLX" (getDay "2022-08-15")
   putStrLn . show $ res
 
--- src/StockPrices/Repository.hs
+++ src/StockPrices/Repository.hs
@@ -1,9 +1,17 @@
-module StockPrices.Repository (retrieveTickerPrice, insertTickerHistory) where
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
+module StockPrices.Repository (retrieveTickerPrice, createTicker) where
 
+import qualified Data.Text as Tt
+import Data.Text.Encoding
 import Database.PostgreSQL.Simple
+import Database.PostgreSQL.Simple.Errors
+import Database.PostgreSQL.Simple.Internal
 import qualified StockPrices.Model.TickerHistory as T
+import qualified Data.ByteString as BS
 import Data.Time
 import GHC.Int
+import Control.Exception
+import Database.PostgreSQL.Simple.Internal
 
 fetchQuery :: Query
 fetchQuery = "SELECT ticker, close, open, low, high, adj_close, volume, date from dbo.ticker_history where ticker = ? and date = ?"
@@ -11,8 +19,34 @@ fetchQuery = "SELECT ticker, close, open, low, high, adj_close, volume, date fro
 insertQuery :: Query
 insertQuery = "INSERT into dbo.ticker_history (ticker, open, high, low, close, adj_close, volume, date) values (?, ?, ?, ?, ?, ?, ?, ?)"
 
-retrieveTickerPrice :: Connection -> String -> Day -> IO [T.TickerHistory]
-retrieveTickerPrice conn tkr date = query conn fetchQuery (tkr :: String, date :: Day)
+retrieveTickerPrice :: Connection -> String -> Day -> IO (Maybe T.TickerHistory)
+retrieveTickerPrice conn tkr date = do
+    xs <- query conn fetchQuery (tkr :: String, date :: Day)
+    case xs of
+        []      -> return $ Nothing
+        (x:[])  -> return $ Just x
 
-insertTickerHistory :: Connection -> T.TickerHistory -> IO GHC.Int.Int64
-insertTickerHistory conn (T.TickerHistory t d a c h l o v) = execute conn insertQuery (t, d, a, c, h, l, o, v)
+createTicker :: Connection -> T.TickerHistory -> IO (Int64)
+createTicker conn (T.TickerHistory t d a c h l o v) = do
+    x <- catchJust constraintViolation (execute conn insertQuery (t, d, a, c, h, l, o, v)) handler
+    return x
+  where
+    handler :: ConstraintViolation -> IO (Int64)
+    handler (NotNullViolation a)      = do
+        print $ "Column can't be null" ++ (bsToStr a)
+        return $ (-1)
+    handler (ForeignKeyViolation t c) = do
+        print $ concat ["FK violation in Table", (bsToStr t), "constraint: ", (bsToStr c) ]
+        return $ (-1)
+    handler (UniqueViolation a)       = do
+        print $ concat ["UniqueViolation: ", (bsToStr a)]
+        return $ (-1)
+    handler (CheckViolation t c)      = do
+        print $ concat ["Check violation in Table", (bsToStr t), "constraint: ", (bsToStr c) ]
+        return $ (-1)
+    handler (ExclusionViolation a)    = do
+        print $ "Excelution violation: " ++ (bsToStr a)
+        return $ (-1)
+
+bsToStr :: BS.ByteString -> String
+bsToStr s = Tt.unpack $ decodeUtf8 s
